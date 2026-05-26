7cf8ae8ba9b83dbf871efbeca4befcc65dd619b9
coloraddict <ajayvjaiswar@gmail.com>
2026-05-26 21:37:04 +0530

    initial commit

---
+++ src/app/app.ts
@@ -0,0 +1,14 @@
+import { Component, signal } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { ButtonModule } from 'primeng/button';
+import { Header } from './components/core/header/header';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet, ButtonModule, Header],
+  templateUrl: './app.html',
+  styleUrl: './app.scss'
+})
+export class App {
+  protected readonly title = signal('O-Bus');
+}
