ceab16d028fb5464c249195d3bba59b19d12054b
coloraddict <ajayvjaiswar@gmail.com>
2026-05-27 16:54:43 +0530

    adds placeholder for booking panel component

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,10 +2,11 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ButtonModule } from 'primeng/button';
 import { Header } from './components/core/header/header';
+import { BookingPanel } from './components/body/booking-panel/booking-panel';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, ButtonModule, Header],
+  imports: [RouterOutlet, ButtonModule, Header, BookingPanel],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
