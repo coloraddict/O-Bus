458e774444b44feaaeba411fbb134cf5b0221ace
coloraddict <ajayvjaiswar@gmail.com>
2026-05-29 00:20:08 +0530

    changes input to autocomplete

--- src/app/components/body/booking-panel/booking-panel.ts
+++ src/app/components/body/booking-panel/booking-panel.ts
@@ -1,31 +1,56 @@
-import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { Component, inject, OnInit, ViewEncapsulation } from '@angular/core';
 import { ToolbarModule } from 'primeng/toolbar';
 import { ButtonModule } from 'primeng/button';
 import { InputTextModule } from 'primeng/inputtext';
 import { FloatLabelModule } from 'primeng/floatlabel';
 import { DatePickerModule } from 'primeng/datepicker';
 import { SelectModule } from 'primeng/select';
-import { City } from '../../../types/city.model';
+import { Country } from '../../../types/city.model';
 import { FormsModule } from '@angular/forms';
+import { BookingPanelService } from '../../../services/booking-panel.service';
+import { AutoCompleteModule } from 'primeng/autocomplete';
+
+interface AutoCompleteCompleteEvent {
+    originalEvent: Event;
+    query: string;
+}
 
 @Component({
   selector: 'app-booking-panel',
-  imports: [ToolbarModule, ButtonModule, InputTextModule, FloatLabelModule, DatePickerModule, SelectModule, FormsModule],
+  imports: [ToolbarModule, ButtonModule, InputTextModule, FloatLabelModule, DatePickerModule, SelectModule, FormsModule, AutoCompleteModule],
   templateUrl: './booking-panel.html',
   styleUrl: './booking-panel.scss',
 })
 export class BookingPanel implements OnInit {
 
-  cities!: City[];
-  selectedCity: City | undefined;
+  country!: Country[];
+  selectedCountry: Country | undefined;
+  cities: any[] = [];
+  cityList: any[] = [];
+  selectedCity: any;
+
+  bookingService = inject(BookingPanelService);
 
   ngOnInit() {
-        this.cities = [
+        this.country = [
             { name: 'New York', code: 'NY' },
             { name: 'Rome', code: 'RM' },
             { name: 'London', code: 'LDN' },
             { name: 'Istanbul', code: 'IST' },
             { name: 'Paris', code: 'PRS' }
         ];
+
+        this.bookingService.getCities().subscribe((res: any) => {
+          this.cityList = res;
+        })
+    }
+
+    search(event: AutoCompleteCompleteEvent) {
+        const query = event.query.toLowerCase();
+        console.log(query)
+        this.cities = this.cityList.filter(city => 
+          city.cityName.toLowerCase().includes(query)
+        );
+        console.log(this.cities);
     }
 }
