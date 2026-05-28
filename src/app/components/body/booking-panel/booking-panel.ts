c018eef4c18ff3b84561ae95c75792f4c13c247a
coloraddict <ajayvjaiswar@gmail.com>
2026-05-28 23:08:34 +0530

    refactors ui

--- src/app/components/body/booking-panel/booking-panel.ts
+++ src/app/components/body/booking-panel/booking-panel.ts
@@ -1,14 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { ToolbarModule } from 'primeng/toolbar';
 import { ButtonModule } from 'primeng/button';
 import { InputTextModule } from 'primeng/inputtext';
 import { FloatLabelModule } from 'primeng/floatlabel';
 import { DatePickerModule } from 'primeng/datepicker';
+import { SelectModule } from 'primeng/select';
+import { City } from '../../../types/city.model';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-booking-panel',
-  imports: [ToolbarModule, ButtonModule, InputTextModule, FloatLabelModule, DatePickerModule],
+  imports: [ToolbarModule, ButtonModule, InputTextModule, FloatLabelModule, DatePickerModule, SelectModule, FormsModule],
   templateUrl: './booking-panel.html',
   styleUrl: './booking-panel.scss',
 })
-export class BookingPanel {}
+export class BookingPanel implements OnInit {
+
+  cities!: City[];
+  selectedCity: City | undefined;
+
+  ngOnInit() {
+        this.cities = [
+            { name: 'New York', code: 'NY' },
+            { name: 'Rome', code: 'RM' },
+            { name: 'London', code: 'LDN' },
+            { name: 'Istanbul', code: 'IST' },
+            { name: 'Paris', code: 'PRS' }
+        ];
+    }
+}
