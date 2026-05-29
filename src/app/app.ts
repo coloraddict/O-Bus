cb65fe3f7d6bc2c7ae89f780d86c0ae5a61369ea
coloraddict <ajayvjaiswar@gmail.com>
2026-05-29 12:40:56 +0530

    adds dashboard page

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,12 +3,13 @@ import { RouterOutlet } from '@angular/router';
 import { ButtonModule } from 'primeng/button';
 import { Header } from './components/core/header/header';
 import { BookingPanel } from './components/body/booking-panel/booking-panel';
+import { Dashboard } from './components/pages/dashboard/dashboard';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, ButtonModule, Header, BookingPanel],
+  imports: [RouterOutlet, ButtonModule, Header, BookingPanel, Dashboard],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrl: './app.scss',
 })
 export class App {
   protected readonly title = signal('O-Bus');
