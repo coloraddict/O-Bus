686c4a23872c43aa6b59139bc3c8b0d80e739953
Ajay Jaiswar <ajayvjaiswar@gmail.com>
2026-05-31 00:11:25 +0530

    add routes for login and register

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { Register } from './components/pages/register/register';
 import { Home } from './components/pages/home/home';
+import { Login } from './components/pages/login/login';
 
 export const routes: Routes = [
   { path: '', component: Home },
   { path: 'register', component: Register },
+  { path: 'login', component: Login },
 ];
