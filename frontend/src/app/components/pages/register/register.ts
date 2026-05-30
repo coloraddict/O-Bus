41cd2e494192b53b6c1647689b05c43ad7a6fa6c
Ajay Jaiswar <ajayvjaiswar@gmail.com>
2026-05-31 00:10:51 +0530

    create register ui

--- frontend/src/app/components/pages/register/register.ts
+++ frontend/src/app/components/pages/register/register.ts
@@ -1,8 +1,12 @@
 import { Component } from '@angular/core';
+import { CardModule } from 'primeng/card';
+import { InputTextModule } from 'primeng/inputtext';
+import { ButtonModule } from 'primeng/button';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-register',
-  imports: [],
+  imports: [CardModule, InputTextModule, ButtonModule, RouterLink],
   templateUrl: './register.html',
   styleUrl: './register.scss',
 })
