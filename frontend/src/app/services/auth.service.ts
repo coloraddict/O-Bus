44272c62e2e1cb8c396e509e2afc4fb2bdd8a908
Ajay Jaiswar <ajayvjaiswar@gmail.com>
2026-06-02 23:36:23 +0530

    adds service for authentication

---
+++ frontend/src/app/services/auth.service.ts
@@ -0,0 +1,13 @@
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthService {
+  http = inject(HttpClient);
+
+  register(user: any) {
+    return this.http.post('http://localhost:3000/api/auth/register', user);
+  }
+}
