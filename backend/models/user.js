75adb4c847b28996df3feec1c1a1d337d5332212
coloraddict <ajayvjaiswar@gmail.com>
2026-06-02 22:56:14 +0530

    adds user type

---
+++ backend/models/user.js
@@ -0,0 +1,11 @@
+const mongoose = require ('mongoose');
+
+const userSchema = mongoose.Schema ({
+  firstName: {type: String, required: true, trim: true},
+  lastName: {type: String, required: true, trim: true},
+  email: {type: String, required: true, unique: true, trim: true},
+  userName: {type: String, required: true, unique: true, trim: true},
+  password: {type: String, required: true},
+});
+
+module.exports = mongoose.model ('User', userSchema);
