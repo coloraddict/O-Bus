b46c82f5489c8e68c31aa1bf9b2981faa5d62e60
Ajay Jaiswar <ajayvjaiswar@gmail.com>
2026-06-02 23:52:14 +0530

    implements login

--- frontend/src/app/components/pages/login/login.ts
+++ frontend/src/app/components/pages/login/login.ts
@@ -1,13 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { CardModule } from 'primeng/card';
 import { InputTextModule } from 'primeng/inputtext';
 import { ButtonModule } from 'primeng/button';
-import { RouterLink } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
+import { AuthService } from '../../../services/auth.service';
+import {
+  FormControl,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  imports: [CardModule, InputTextModule, ButtonModule, RouterLink],
+  imports: [
+    CardModule,
+    InputTextModule,
+    ButtonModule,
+    RouterLink,
+    FormsModule,
+    ReactiveFormsModule,
+  ],
   templateUrl: './login.html',
   styleUrl: './login.scss',
 })
-export class Login {}
+export class Login {
+  authService = inject(AuthService);
+
+  loginForm!: FormGroup;
+
+  constructor(private router: Router) {
+    this.loginForm = new FormGroup({
+      email: new FormControl('', [Validators.required]),
+      password: new FormControl('', [Validators.required]),
+    });
+  }
+
+  onLogin() {
+    this.authService.login(this.loginForm.value).subscribe((response) => {
+      this.router.navigateByUrl('');
+    });
+  }
+}
