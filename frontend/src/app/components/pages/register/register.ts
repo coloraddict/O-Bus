3f93453a705972ab09e6fbb3c4b8bea51319ab76
Ajay Jaiswar <ajayvjaiswar@gmail.com>
2026-06-02 23:36:59 +0530

    implements register func

--- frontend/src/app/components/pages/register/register.ts
+++ frontend/src/app/components/pages/register/register.ts
@@ -1,13 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { CardModule } from 'primeng/card';
 import { InputTextModule } from 'primeng/inputtext';
 import { ButtonModule } from 'primeng/button';
-import { RouterLink } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
+import {
+  FormControl,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { AuthService } from '../../../services/auth.service';
 
 @Component({
   selector: 'app-register',
-  imports: [CardModule, InputTextModule, ButtonModule, RouterLink],
+  imports: [
+    CardModule,
+    InputTextModule,
+    ButtonModule,
+    RouterLink,
+    FormsModule,
+    ReactiveFormsModule,
+  ],
   templateUrl: './register.html',
   styleUrl: './register.scss',
 })
-export class Register {}
+export class Register {
+  authService = inject(AuthService);
+
+  constructor(private router: Router) {}
+
+  registerForm = new FormGroup({
+    firstName: new FormControl('', [Validators.required]),
+    lastName: new FormControl('', [Validators.required]),
+    email: new FormControl('', [Validators.required]),
+    userName: new FormControl('', [Validators.required]),
+    password: new FormControl('', [Validators.required]),
+  });
+
+  onRegister() {
+    this.authService.register(this.registerForm.value).subscribe((response) => {
+      this.router.navigateByUrl('/login');
+    });
+  }
+}
