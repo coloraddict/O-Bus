955325b29b1844fd55870d498edbe2fba6462f9a
Ajay Jaiswar <ajayvjaiswar@gmail.com>
2026-06-03 17:20:19 +0530

    adds validation

--- frontend/src/app/components/pages/register/register.ts
+++ frontend/src/app/components/pages/register/register.ts
@@ -12,7 +12,9 @@ import {
   Validators,
 } from '@angular/forms';
 import { AuthService } from '../../../services/auth.service';
-import { maxLength } from '@angular/forms/signals';
+import { passwordMatchValidator } from '../../../utils/password.validator';
+import { NgClass } from '@angular/common';
+import { PasswordModule } from 'primeng/password';
 
 @Component({
   selector: 'app-register',
@@ -23,6 +25,8 @@ import { maxLength } from '@angular/forms/signals';
     RouterLink,
     FormsModule,
     ReactiveFormsModule,
+    NgClass,
+    PasswordModule,
   ],
   templateUrl: './register.html',
   styleUrl: './register.scss',
@@ -30,24 +34,62 @@ import { maxLength } from '@angular/forms/signals';
 export class Register {
   authService = inject(AuthService);
   registerForm!: FormGroup;
+  showPassword = false;
+  showConfirmPassword = false;
 
   constructor(
     private router: Router,
     private fb: FormBuilder,
   ) {
-    this.registerForm = this.fb.group({
-      firstName: ['', Validators.required],
-      lastName: ['', Validators.required],
-      email: ['', Validators.required],
-      userName: ['', Validators.required],
-      password: ['', Validators.required],
-      confirmPassword: ['', Validators.required],
-    });
+    this.registerForm = this.fb.group(
+      {
+        firstName: ['', [Validators.required, Validators.minLength(3)]],
+        lastName: ['', [Validators.required, Validators.minLength(3)]],
+        email: [
+          '',
+          [Validators.required, Validators.email, Validators.pattern(/^[^\s@]+@[^\s@]+\.[^\s@]+$/)],
+        ],
+        userName: ['', [Validators.required, Validators.minLength(4), Validators.maxLength(20)]],
+        password: [
+          '',
+          [
+            Validators.required,
+            Validators.minLength(8),
+            Validators.pattern(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&]).{8,}$/),
+          ],
+        ],
+        confirmPassword: ['', [Validators.required]],
+      },
+      { validators: passwordMatchValidator },
+    );
   }
 
   onRegister() {
+    if (this.registerForm.invalid) {
+      this.registerForm.markAllAsTouched();
+      return;
+    }
     this.authService.register(this.registerForm.value).subscribe((response) => {
       this.router.navigateByUrl('/login');
     });
   }
+
+  get f() {
+    return this.registerForm.controls;
+  }
+
+  // passwordMatchValidator(group: FormGroup) {
+  //   const password = group.get('password')?.value;
+  //   const confirmPassword = group.get('confirmPassword')?.value;
+
+  //   if (password !== confirmPassword) {
+  //     group.get('confirmPassword')?.setErrors({
+  //       passwordMismatch: true,
+  //     });
+  //   } else {
+  //     group.get('confirmPassword')?.setErrors(null);
+  //   }
+
+  //   return null;
+  // }
 }
