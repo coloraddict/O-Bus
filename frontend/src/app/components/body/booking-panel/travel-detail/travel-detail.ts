88bae965389764603c1c99184e191b2d39e9e100
Ajay Jaiswar <ajayvjaiswar@gmail.com>
2026-06-08 22:50:19 +0530

    implements reactive form

--- frontend/src/app/components/body/booking-panel/travel-detail/travel-detail.ts
+++ frontend/src/app/components/body/booking-panel/travel-detail/travel-detail.ts
@@ -1,15 +1,112 @@
 import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import {
+  FormArray,
+  FormBuilder,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+} from '@angular/forms';
 import { ButtonModule } from 'primeng/button';
 import { SelectModule } from 'primeng/select';
 
 @Component({
   selector: 'app-travel-detail',
-  imports: [ButtonModule, SelectModule, FormsModule],
+  imports: [ButtonModule, SelectModule, FormsModule, ReactiveFormsModule],
   templateUrl: './travel-detail.html',
   styleUrl: './travel-detail.scss',
 })
 export class TravelDetail {
-  value1: any;
-  cityList: any[] = [];
+  adultFormGroup!: FormGroup;
+  youthFormGroup!: FormGroup;
+  seniorFormGroup!: FormGroup;
+
+  adultAgeList: any = [];
+  youthAgeList: any = [];
+  seniorAgeList: any = [];
+
+  constructor(private fb: FormBuilder) {
+    this.adultFormGroup = this.fb.group({
+      adults: this.fb.array([this.createAdult()]),
+    });
+    this.youthFormGroup = this.fb.group({
+      youths: this.fb.array([this.createYouth()]),
+    });
+    this.seniorFormGroup = this.fb.group({
+      seniors: this.fb.array([this.createSenior()]),
+    });
+  }
+
+  ngOnInit() {
+    for (let i = 26; i <= 57; i++) {
+      this.adultAgeList.push({ age: i + ' years' });
+    }
+
+    for (let i = 0; i <= 25; i++) {
+      this.youthAgeList.push({ age: i + ' years' });
+    }
+
+    for (let i = 58; i < 100; i++) {
+      this.seniorAgeList.push({ age: i + ' years' });
+    }
+  }
+
+  get adults(): FormArray {
+    return this.adultFormGroup.get('adults') as FormArray;
+  }
+
+  get youths(): FormArray {
+    return this.youthFormGroup.get('youths') as FormArray;
+  }
+
+  get seniors(): FormArray {
+    return this.seniorFormGroup.get('seniors') as FormArray;
+  }
+
+  createAdult(): FormGroup {
+    return this.fb.group({
+      age: [null],
+    });
+  }
+
+  addAdult() {
+    this.adults.push(this.createAdult());
+  }
+
+  removeAdult() {
+    if (this.adults.length > 1) {
+      this.adults.removeAt(this.adults.length - 1);
+    }
+  }
+
+  createYouth(): FormGroup {
+    return this.fb.group({
+      age: [null],
+    });
+  }
+
+  addYouth() {
+    this.youths.push(this.createYouth());
+  }
+
+  removeYouth() {
+    if (this.youths.length > 1) {
+      this.youths.removeAt(this.youths.length - 1);
+    }
+  }
+
+  createSenior(): FormGroup {
+    return this.fb.group({
+      age: [null],
+    });
+  }
+
+  addSenior() {
+    this.seniors.push(this.createSenior());
+  }
+
+  removeSenior() {
+    if (this.seniors.length > 1) {
+      this.seniors.removeAt(this.seniors.length - 1);
+    }
+  }
 }
