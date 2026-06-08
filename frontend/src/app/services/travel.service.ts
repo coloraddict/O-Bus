d607dd80b26f75f5c2f46082f200d1d0edbac16e
Ajay Jaiswar <ajayvjaiswar@gmail.com>
2026-06-08 23:34:49 +0530

    adds new service to manage passanger count in booking panel

---
+++ frontend/src/app/services/travel.service.ts
@@ -0,0 +1,36 @@
+import { computed, Injectable, signal } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class TravelService {
+  readonly adultCount = signal<number>(0);
+  readonly youthCount = signal<number>(0);
+  readonly seniorCount = signal<number>(0);
+
+  readonly total = computed(() => this.adultCount() + this.youthCount() + this.seniorCount());
+
+  increment(category: string) {
+    if (category === 'adult') {
+      this.adultCount.update((c) => c + 1);
+    }
+    if (category === 'youth') {
+      this.youthCount.update((c) => c + 1);
+    }
+    if (category === 'senior') {
+      this.seniorCount.update((c) => c + 1);
+    }
+  }
+
+  decrement(category: string) {
+    if (category === 'adult') {
+      this.adultCount.update((c) => c - 1);
+    }
+    if (category === 'youth') {
+      this.youthCount.update((c) => c - 1);
+    }
+    if (category === 'senior') {
+      this.seniorCount.update((c) => c - 1);
+    }
+  }
+}
