0f68f0a13169f955de1647fd25dfc76a9f87f02d
Ajay Jaiswar <ajayvjaiswar@gmail.com>
2026-06-09 21:28:05 +0530

    introduces new route "search"

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { Register } from './components/pages/register/register';
 import { Home } from './components/pages/home/home';
 import { Login } from './components/pages/login/login';
+import { Search } from './components/pages/search/search';
 
 export const routes: Routes = [
   { path: '', component: Home },
   { path: 'register', component: Register },
   { path: 'login', component: Login },
+  { path: 'search', component: Search },
 ];
